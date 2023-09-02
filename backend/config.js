dae5d25234fda3a5e8cad1c5517de07dd8b470d8
Uddhika <uddhikaishara@gmail.com>
2023-09-02 10:53:41 +0530

    updated

---
+++ backend/config.js
@@ -0,0 +1,2 @@
+export const PORT = 5555;
+export const mongoDBURL='mongodb+srv://uddhika:1234@cluster0.chybuzw.mongodb.net/books-collection?retryWrites=true&w=majority'
---
+++ backend/index.js
@@ -0,0 +1,53 @@
+import express, { request, response } from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
+
+const app = express();
+
+app.use(express.json());
+
+app.post('/books', async (request, response) => {
+    try{
+        if(
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ){
+            return response.status(400).send({
+                message: 'Send all required fields: title, author, publishYear',
+            });
+        }
+        const newBook = {
+            title: request.body.title,
+            author: request.body.author,
+            publishYear: request.body.publishYear,
+        };
+
+        const book = await Book.create(newBook);
+
+        return response.status(201).send(book);
+    }
+    catch(error){
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }
+});
+
+app.get('/books', async (request,response) => {
+    try{
+        cos
+    }catch(error){
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }
+});
+
+mongoose.connect(mongoDBURL).then(() => {
+    console.log('App Connected to database')
+    app.listen(PORT, () => {
+        console.log(`App is listening to the port: ${PORT}`)
+    });
+}).catch((error) => {
+    console.log('not connected');
+});
