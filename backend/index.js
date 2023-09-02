d9a87dc30d702dfab1d465488bb4ce70b0205b36
Uddhika <uddhikaishara@gmail.com>
2023-09-02 13:35:58 +0530

    routers updated

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,13 @@ import express, { request, response } from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import bookRoute from './routes/bookRoute.js';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
 
 app.use('/books', bookRoute);
 
